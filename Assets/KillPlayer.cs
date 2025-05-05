41e90e3aa7eb67f1c058deec20087bdf75a84796
sclaude <claude.sabinotto@ynov.com>
2025-05-05 13:45:02 +0200

    Preview

---
+++ Assets/KillPlayer.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KillPlayer : MonoBehaviour
+{
+    public GameObject player;
+    public Transform respawnPoint;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+  private void OnCollisionEnter2D(Collision2D other) 
+  {
+    if(other.gameObject.CompareTag("Player"))
+    {
+        player.transform.position = respawnPoint.position;
+        //Scene currentScene = SceneManager.GetActiveScene();;
+        //SceneManager.LoadScene(currentScene.name);
+    }
+  }
+
+}
